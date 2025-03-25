72c575a27727e6ee1fe5b6b461ab54ae457d0695
Oren Teich <oren@teich.net>
2025-03-25 11:12:04 -0700

    cleanup

--- import_items.sh
+++ import_items.sh
@@ -4,6 +4,7 @@
 # Using --full-refresh to ensure all data is loaded
 echo "Running import for items from /Users/oren/Dropbox-AAC/AAC/Oren/CSV/item_all.csv"
 echo "Step 1: Extract and load items data"
+export ITEMS_FILE_PATH="/Users/oren/Dropbox-AAC/AAC/Oren/CSV/item_all.csv"
 meltano run --full-refresh tap-csv-items target-postgres
 
 # Run DBT to transform the data into the products table and track history
